9710e5c48053319293ccc5cf5e867cecb0c61bd4
axju <axel.juraske@short-report.de>
2019-02-08 21:40:51 +0100

    split util

---
+++ besttags/util/__init__.py
@@ -0,0 +1,4 @@
+from besttags.util.func import merge_list, merge_dict, limit
+from besttags.util.cls import Tags, BasicManager
+
+__all__ = ['merge_list', 'merge_dict', 'limit', 'Tags', 'BasicManager']
